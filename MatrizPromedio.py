e896d5da3ba3e6582a83a483fea659116c6e9665
StevenCM199 <36709204+StevenCM199@users.noreply.github.com>
2018-05-09 14:31:11 -0600

    Update MatrizPromedio.py

--- MatrizPromedio.py
+++ MatrizPromedio.py
@@ -5,14 +5,14 @@ def suma_matriz(matriz):
 
 def matriz_aux(matriz, num_filas, num_columnas, fila, columna, resultado):
     if fila == len(matriz):
-        return promedio(resultado, len(matriz), 0)
+        return promedio(resultado, (len(matriz)*len(matriz[0])),0)
     elif columna == len(matriz[0]):
         return matriz_aux(matriz, num_filas, num_columnas, fila + 1 , 0, resultado)
     else: return matriz_aux(matriz, num_filas, num_columnas, fila, columna + 1, resultado + matriz[fila][columna])
 
-def promedio(suma, num_filas, resultado):
+def promedio(suma, num_datos, resultado):
     if suma == 0:
         return suma, resultado
     else:
-        return suma, resultado + suma / num_filas
+        return suma, resultado + suma / num_datos
     
