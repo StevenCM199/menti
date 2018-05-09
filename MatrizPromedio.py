b659971a9bdc8f1e86fcee93d9badfb8f8c6b62e
StevenCM199 <36709204+StevenCM199@users.noreply.github.com>
2018-05-09 13:48:00 -0600

    Add files via upload

---
+++ MatrizPromedio.py
@@ -0,0 +1,18 @@
+def suma_matriz(matriz):
+    if isinstance(matriz,list) and matriz !=[]:
+        return matriz_aux(matriz, len(matriz), len(matriz[0]) ,0,0,0)
+    else:return "La matriz no es una lista"
+
+def matriz_aux(matriz, num_filas, num_columnas, fila, columna, resultado):
+    if fila == len(matriz):
+        return promedio(resultado, len(matriz), 0)
+    elif columna == len(matriz[0]):
+        return matriz_aux(matriz, num_filas, num_columnas, fila + 1 , 0, resultado)
+    else: return matriz_aux(matriz, num_filas, num_columnas, fila, columna + 1, resultado + matriz[fila][columna])
+
+def promedio(suma, num_filas, resultado):
+    if suma == 0:
+        return resultado
+    else:
+        return resultado + suma / num_filas
+    
